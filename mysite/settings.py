702ac8c3c1787bae16f463fa110a1f8564bcc930
kaliniushko <kaliniushko.prog@gmail.com>
2020-12-07 23:41:12 +0200

    Changed settings.py(STATIC_ROOT)(7)

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_DIRS = (os.path.join(BASE_DIR, 'static'), '/home/kaliniushko/djangogirls/static', '/home/kaliniushko/djangogirls/blog/static/css', )
